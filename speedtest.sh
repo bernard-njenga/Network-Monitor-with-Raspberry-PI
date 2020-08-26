367febfa03e6d355511bfc8f5075217f1e782c2a
bernard-njenga <70279046+bernard-njenga@users.noreply.github.com>
2020-08-26 18:48:43 +0300

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -1,9 +1,12 @@
 #*/bin/bash
 
+#set minimal threshold to go to the next step
 minimal=0
-#ping=$(ping -c 1 -W 1 www.google.com |egrep -F 'ttl=') 
+
+#ping www.google.com with 10 retrials 
 ping=$(ping -c 10 -W 1 www.google.com |grep -c 'ttl=*')
 
+#if else statement to continue to next script if output is greater than 0 or less than 0
 if [ $ping -gt $minimal ];
 then
     echo "host reachable"
